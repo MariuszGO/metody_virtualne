c8e0d0b780bead3f5a7e73ce4a91eaf92469d8ea
MariuszGO <75947509+MariuszGO@users.noreply.github.com>
2023-12-13 11:37:10 +0100

    Dodaj pliki projektów.

---
+++ metody_virtualne/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace metody_virtualne
+{
+    public class Samochod
+    {
+        public string marka;
+        public string model;
+        public int rok;
+    
+    public virtual void wyswietl()
+        {
+            Console.WriteLine("To jest samochod" + marka + " " + model);
+        }
+    
+    }
+
+    public class Osobowy : Samochod
+    {
+        public override void wyswietl()
+        {
+            Console.WriteLine("To jest samochod osobowy: " + marka + " " + model);
+        }
+    }
+
+    public class Ciezarowka : Samochod
+    {
+        public override void wyswietl()
+        {
+            Console.WriteLine("To jest samochod ciężarowy: " + marka + " " + model);
+        }
+    }
+
+    public class Autobus : Samochod
+    {
+        public override void wyswietl()
+        {
+            Console.WriteLine("To jest autobus: " + marka + " " + model);
+        }
+    }
+
+
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            Samochod a1 = new Osobowy();
+            Samochod b1 = new Ciezarowka();
+            Samochod c1 = new Autobus();
+
+
+            a1.wyswietl();
+            b1.wyswietl();
+            c1.wyswietl();
+
+
+        }
+    }
+}
